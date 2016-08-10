c9fd18d369ccb74395dfa14e82098390dda6b413
loupgarou81 <leloupgarou81@gmail.com>
2016-08-10 18:09:27 +0100

    Fixed typings error when npm start

---
+++ app/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// angular browser
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+// aplication module
+var app_module_1 = require('./app.module');
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+//# sourceMappingURL=main.js.map
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+// angular browser
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// aplication module
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
